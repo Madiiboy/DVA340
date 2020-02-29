86ef671bc8cbdfacff379403ea7f3901706e1cb2
Madiiboy <mats.fridberg@gmail.com>
2020-03-01 00:11:42 +0100

    Progress

--- Lab3/antz.py
+++ Lab3/antz.py
@@ -1,5 +1,9 @@
 import math
 import random
+import numpy as np
+from matplotlib import pyplot as plt
+
+best = float('inf')
 
 class City:
     def __init__(self, id, x, y):
@@ -13,11 +17,6 @@ class Ant:
         self.name = name
         self.current = current
         self.visited = visited
-
-class Edge:
-    def __init__(self, distance, pheronone):
-        self.distance = distance
-        self.pheronone = pheronone
 
 def readFromFile():
     file = open('tsp.txt', 'r')
@@ -40,27 +39,63 @@ def readFromFile():
 def calcDistance(a, b):
     return float(math.sqrt((((b.x - a.x) ** 2) + (b.y - a.y) ** 2)))
 
-    # print(len(ants))
-    # return ants
-    # return [ant for i, ant = Ant(i+1,cities[0],[]) in enumerate(amount)]
-
-def initPopulation(amount, cities):
+def initAnts(amount, cities):
     ants = []
     for i in range(amount):
-        ants.append(Ant(i+1,cities[0], cities[0]))
+        ants.append(Ant(i,cities[0], cities[0]))
 
     return ants
+
+# Returns a matrix with distances between all the different cities
+def initCities(cities):
+
+    matrix = np.zeros(shape=(52,52))
+    for i in range(len(cities)):
+        for j in range(len(cities)):
+            if i != j:
+                matrix[i][j] = 1 / float(calcDistance(cities[i], cities[j]))
+            #Distance to same city should be 0
+            else:
+                matrix[i][j] = 0
+
+    # plt.imshow(matrix, interpolation='nearest')
+    # plt.show()
+
+    return matrix
+
+# Returns a matrix of starting values of pheromone
+def initPheromone():
+    matrix = np.zeros(shape=(52,52))
+    #Fill the new matrix with the start value
+    for i in range(52):
+        for j in range(52):
+            if i != j:
+                matrix[i][j] = 10
+            #Distance to same city should be 0
+            else:
+                matrix[i][j] = 0
+    #plt.imshow(matrix, interpolation='nearest')
+    #plt.show()
+
+    return matrix
+
+#def reducePheromone():
+
+#Here we randomly select path taking probabilities into consideration
+def selectPath(ant, cx, ph):
+    print("What we wanna do here?!")
 
 if __name__ == "__main__":
 
     cities = readFromFile()
 
     #initiate ant colony
-    ants = initPopulation(100, cities)
-
+    ants = initAnts(100, cities)
+    cx = initCities(cities)
+    ph = initPheromone()
+    selectPath(ants[0], cx, ph)
     # for a in ants:
     #     print(a.current.id)
-
 
     # !!! Skapa typ 100 myror   !!!!!!
     # !!! Alla myror startar i samma stad !!!!!
